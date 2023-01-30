8a5acaa73ed73020f18a53a71929ddefca630bb2
Dan Moore <dan@fusionauth.io>
2023-01-30 15:29:04 -0700

    Use two spaces for indentation

--- java/src/main/java/io/fusionauth/example/AddMagicLink.java
+++ java/src/main/java/io/fusionauth/example/AddMagicLink.java
@@ -12,24 +12,24 @@ import io.fusionauth.domain.api.ApplicationResponse;
 
 public class AddMagicLink {
 
-    public static void main(String[] args) {
-        final String apiKey = System.getProperty("fusionauth.api.key");
-        final FusionAuthClient client = new FusionAuthClient(apiKey, "http://localhost:9011");
+  public static void main(String[] args) {
+    final String apiKey = System.getProperty("fusionauth.api.key");
+    final FusionAuthClient client = new FusionAuthClient(apiKey, "http://localhost:9011");
 
-        // enable magic links
-        UUID clientId = UUID.fromString(Setup.APPLICATION_ID);
-        
-        Map<String, Object> applicationMap = new HashMap<String, Object>();
-        Map<String, Object> passwordlessConfigurationMap = new HashMap<String, Object>();
-        Map<String, Object> enableMagicLinkUpdateMap = new HashMap<String, Object>();
-        
-        passwordlessConfigurationMap.put("enabled",true);
-        applicationMap.put("passwordlessConfiguration", passwordlessConfigurationMap);
-        enableMagicLinkUpdateMap.put("application", applicationMap);
+    // enable magic links
+    UUID clientId = UUID.fromString(Setup.APPLICATION_ID);
 
-        ClientResponse<ApplicationResponse, Errors> applicationResponse = client.patchApplication(clientId, enableMagicLinkUpdateMap);
-        if (!applicationResponse.wasSuccessful()) {
-        	throw new RuntimeException("couldn't update application");
-        }
+    Map<String, Object> applicationMap = new HashMap<String, Object>();
+    Map<String, Object> passwordlessConfigurationMap = new HashMap<String, Object>();
+    Map<String, Object> enableMagicLinkUpdateMap = new HashMap<String, Object>();
+
+    passwordlessConfigurationMap.put("enabled",true);
+    applicationMap.put("passwordlessConfiguration", passwordlessConfigurationMap);
+    enableMagicLinkUpdateMap.put("application", applicationMap);
+
+    ClientResponse<ApplicationResponse, Errors> applicationResponse = client.patchApplication(clientId, enableMagicLinkUpdateMap);
+    if (!applicationResponse.wasSuccessful()) {
+      throw new RuntimeException("couldn't update application");
     }
+  }
 }
--- java/src/main/java/io/fusionauth/example/AddTwitterOAuth.java
+++ java/src/main/java/io/fusionauth/example/AddTwitterOAuth.java
@@ -15,29 +15,29 @@ import io.fusionauth.domain.provider.TwitterIdentityProvider;
 
 public class AddTwitterOAuth {
 
-    public static void main(String[] args) {
-        final String apiKey = System.getProperty("fusionauth.api.key");
-        final FusionAuthClient client = new FusionAuthClient(apiKey, "http://localhost:9011");
-        
-        // create a twitter provider
-        UUID clientId = UUID.fromString(Setup.APPLICATION_ID);
-        TwitterIdentityProvider twitterIdentityProvider = new TwitterIdentityProvider();
-        twitterIdentityProvider.enabled = true;
-        twitterIdentityProvider.buttonText = "Login With Twitter";
-        twitterIdentityProvider.consumerKey = "change-this-in-production-to-be-a-real-twitter-key";
-        twitterIdentityProvider.consumerSecret = "change-this-in-production-to-be-a-real-twitter-secret";
-        
-        // enable it for our application
-        twitterIdentityProvider.applicationConfiguration = new HashMap<UUID, TwitterApplicationConfiguration>();
-        TwitterApplicationConfiguration twitterApplicationConfiguration = new TwitterApplicationConfiguration();
-        twitterApplicationConfiguration.enabled = true;
-        twitterIdentityProvider.applicationConfiguration.put(clientId, twitterApplicationConfiguration);
+  public static void main(String[] args) {
+    final String apiKey = System.getProperty("fusionauth.api.key");
+    final FusionAuthClient client = new FusionAuthClient(apiKey, "http://localhost:9011");
 
-        IdentityProviderRequest twitterIdentityProviderRequest = new IdentityProviderRequest(twitterIdentityProvider);
+    // create a twitter provider
+    UUID clientId = UUID.fromString(Setup.APPLICATION_ID);
+    TwitterIdentityProvider twitterIdentityProvider = new TwitterIdentityProvider();
+    twitterIdentityProvider.enabled = true;
+    twitterIdentityProvider.buttonText = "Login With Twitter";
+    twitterIdentityProvider.consumerKey = "change-this-in-production-to-be-a-real-twitter-key";
+    twitterIdentityProvider.consumerSecret = "change-this-in-production-to-be-a-real-twitter-secret";
+
+    // enable it for our application
+    twitterIdentityProvider.applicationConfiguration = new HashMap<UUID, TwitterApplicationConfiguration>();
+    TwitterApplicationConfiguration twitterApplicationConfiguration = new TwitterApplicationConfiguration();
+    twitterApplicationConfiguration.enabled = true;
+    twitterIdentityProvider.applicationConfiguration.put(clientId, twitterApplicationConfiguration);
+
+    IdentityProviderRequest twitterIdentityProviderRequest = new IdentityProviderRequest(twitterIdentityProvider);
 		
-        ClientResponse<IdentityProviderResponse, Errors> identityProviderResponse = client.createIdentityProvider(null, twitterIdentityProviderRequest);
-        if (!identityProviderResponse.wasSuccessful()) {
-        	throw new RuntimeException("couldn't add twitter OAuth");
-        }
+    ClientResponse<IdentityProviderResponse, Errors> identityProviderResponse = client.createIdentityProvider(null, twitterIdentityProviderRequest);
+    if (!identityProviderResponse.wasSuccessful()) {
+      throw new RuntimeException("couldn't add twitter OAuth");
     }
+  }
 }
